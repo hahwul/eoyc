181b07d374e090e30be96fc763d2c439c76d1458
hahwul <hahwul@gmail.com>
2023-03-18 00:45:41 +0900

    Add URL En/Decode and up/downcase

--- src/encoders.cr
+++ src/encoders.cr
@@ -1,6 +1,7 @@
 require "base64"
 require "digest/md5"
 require "digest/sha1"
+require "uri"
 
 def encode(str, encoders)
     result = ""
@@ -15,6 +16,14 @@ def encode(str, encoders)
             result = Digest::MD5.hexdigest(str.to_s)
         when "sha1"
             result = Digest::SHA1.base64digest(str.to_s)
+        when "url", "url-encode"
+            result = URI.encode_www_form(str.to_s)
+        when "url-decode"
+            result = URI.decode_www_form(str.to_s)
+        when "upcase"
+            result = str.to_s.upcase
+        when "downcase"
+            result = str.to_s.downcase
         else
             result = str
         end 
