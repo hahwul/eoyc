caf62ae6a2adee39b71d0deaab314ebd62168d47
HAHWUL <hahwul@gmail.com>
2025-09-14 23:21:16 +0900

    Reindent all source and spec files for consistency

--- spec/utils_spec.cr
+++ spec/utils_spec.cr
@@ -2,15 +2,15 @@ require "spec"
 require "./spec_helper"
 
 describe "find" do
-    it "returns the correct value" do
-        result = find("abcd", "bc")
-        result.not_nil![0].should eq("bc")
-    end
+  it "returns the correct value" do
+    result = find("abcd", "bc")
+    result.not_nil![0].should eq("bc")
+  end
 end
 
 describe "replace" do
-    it "returns the correct value" do
-        result = replace("abcd", "bc", "ef")
-        result.should eq("aefd")
-    end
+  it "returns the correct value" do
+    result = replace("abcd", "bc", "ef")
+    result.should eq("aefd")
+  end
 end
--- src/encoders.cr
+++ src/encoders.cr
@@ -6,39 +6,39 @@ require "digest/sha512"
 require "uri"
 
 def encode(str, encoders)
-    result = ""
-    if encoders.size > 0 && str != ""
-        target = encoders.pop
-        case target.strip
-        when "base64", "base64-encode"
-            result = Base64.encode(str.to_s)
-        when "base64-decode"
-            result = Base64.decode(str.to_s)
-        when "md5"
-            result = Digest::MD5.hexdigest(str.to_s)
-        when "sha1"
-            result = Digest::SHA1.base64digest(str.to_s)
-        when "sha256"
-            result = Digest::SHA256.base64digest(str.to_s)
-        when "sha512"
-            result = Digest::SHA512.base64digest(str.to_s)
-        when "url", "url-encode"
-            result = URI.encode_www_form(str.to_s)
-        when "url-decode"
-            result = URI.decode_www_form(str.to_s)
-        when "upcase"
-            result = str.to_s.upcase
-        when "downcase"
-            result = str.to_s.downcase
-        when "redacted", "redaction"
-            size = str.to_s.size
-            result = "◼" * size
-        else
-            result = str
-        end 
-        result = encode(result, encoders)
-    else 
-        result = str
+  result = ""
+  if encoders.size > 0 && str != ""
+    target = encoders.pop
+    case target.strip
+    when "base64", "base64-encode"
+      result = Base64.encode(str.to_s)
+    when "base64-decode"
+      result = Base64.decode(str.to_s)
+    when "md5"
+      result = Digest::MD5.hexdigest(str.to_s)
+    when "sha1"
+      result = Digest::SHA1.base64digest(str.to_s)
+    when "sha256"
+      result = Digest::SHA256.base64digest(str.to_s)
+    when "sha512"
+      result = Digest::SHA512.base64digest(str.to_s)
+    when "url", "url-encode"
+      result = URI.encode_www_form(str.to_s)
+    when "url-decode"
+      result = URI.decode_www_form(str.to_s)
+    when "upcase"
+      result = str.to_s.upcase
+    when "downcase"
+      result = str.to_s.downcase
+    when "redacted", "redaction"
+      size = str.to_s.size
+      result = "◼" * size
+    else
+      result = str
     end
-    return result.to_s.strip
+    result = encode(result, encoders)
+  else
+    result = str
+  end
+  result.to_s.strip
 end
